38eb3b5d5e8a956abb2adc65a38a04106e712ba8
David Okih <98276781+Davidokih@users.noreply.github.com>
2022-06-20 12:48:47 +0100

    Update cloudinary.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: "353622814343242",
-    api_secret: process.env.CLOUD_SECRET,
+    api_secret: "QeAuML_hstw5s3RDsI8uAl-hyMc",
     secure: true
 });
 
